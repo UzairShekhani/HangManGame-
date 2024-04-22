af6c77e0e6763a99ec5e62071506662511331062
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-04-22 13:50:00 +0500

    Js updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
 
-let option = {
+let options = {
     fruits: [
         "Apple",
         "Blueberry",
@@ -42,17 +42,39 @@ const displayOption = () => {
     optionContainer.innerHTML += '<h3>Please Select An Option</h3>';
 
     let buttonCon = document.createElement("div");
-    for (let value in option) {
+    for (let value in options) {
         buttonCon.innerHTML += `<button class="option" onclick="generateWord('${value}')">${value}</button>`;
     }
     optionContainer.appendChild(buttonCon);
 };
 
+
+const generateWord = (optionValue) => {
+    let optionButtons = document.querySelectorAll(".option")
+
+    optionButtons.forEach((button) => {
+        if(button.innerText.toLowerCase() === optionValue){
+            button.classList.add("active")
+        }
+        button.disabled = true
+    })
+}
+
+
 const initializer = () => {
     winCount = 0;
     Count = 0;
+
+    for (let i = 65; i < 91; i++) {
+        let button = document.createElement("button");
+        button.classList.add("letters");
+        button.innerText = String.fromCharCode(i);
+        letterContainer.append(button);
+    }
+
     displayOption();
 };
+
 
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
